b6b9412504be31ef38301dc55c7edd0502c74126
Suzi Rubino <suzi@suzirubi.com>
2016-10-17 23:06:40 -0700

    Add javascript for cooking conversions

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var gallons = parseFloat(prompt("Enter number of gallons you would like to convert: "));
+
+alert(gallons + " gallons converted to liters equals " + gallons*3.7851 + " liters.");
+alert(gallons + " gallons converted to ounces equals " + gallons*128 + " ounces.")
+
+var cups = parseFloat(prompt("Enter number of cups you would like to convert: "));
+
+alert(cups + " cups converted to tablespoons equals " + cups*19.2152 + " tablespoons.");
+alert(cups + " cups converted to ounces equals " + gallons*0.520421 + " ounces.");
